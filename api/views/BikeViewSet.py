396e2b3b40a81a383276f310fbf9c46f7c3b2834
silvestre <silvestre@devslot>
2024-02-01 08:37:45 +0700

    display products in reverse order

--- api/views/BikeViewSet.py
+++ api/views/BikeViewSet.py
@@ -4,6 +4,6 @@ from api.models import Bike
 
 class BikeViewSet(viewsets.ModelViewSet):
     """Listing all registered bikes in stock"""
-    queryset = Bike.objects.all()
+    queryset = Bike.objects.all().order_by('-id')
     serializer_class = BikeSerializer
 
