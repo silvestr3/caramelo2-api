e614f512818b74372c9e18393fdbb5dc50cc063b
silvestr3 <fellipe.silvestre@outlook.com>
2024-02-05 15:25:19 +0700

    implement order checkout

--- api/views/OrderViewSet.py
+++ api/views/OrderViewSet.py
@@ -1,9 +1,48 @@
 from rest_framework import viewsets
 from api.serializers import OrderSerializer
-from api.models import Order
+from api.models import Order, Customer, Bike, AdditionalFee
+from django.utils.timezone import datetime
+
+from rest_framework.response import Response
 
 class OrderViewSet(viewsets.ModelViewSet):
     """Listing all registered Sales"""
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def create(self, request):
+        customer = Customer.objects.filter(pk=request.data["customer"])[0]
+
+        new_order = Order.objects.create(
+            sale_date= datetime.today(),
+            customer= customer,
+
+            total_price= request.data['total'],
+            discount= request.data['discount'],
+            down_payment= request.data['down_payment'],
+            total=request.data['total'],
+
+            payment_method=request.data['payment_method'],
+            notes= request.data['notes'],
+            registration_status= 'CPL',
+            has_checkout= True
+        )
+
+        for bike in request.data['bikes']:
+            instance = Bike.objects.filter(pk=bike)[0]
+            instance.sold = True
+            instance.save()
+            new_order.bikes.add(instance)
+
+        for fee in request.data['additional_fees']:
+            instance = AdditionalFee.objects.create(
+                description=fee["description"],
+                amount=fee['amount']
+            )
+
+            new_order.additional_fees.add(instance)
+            instance.save()
+        
+        new_order.save()
+
+        return Response({"success": True, "message": "order placed successfully"})
