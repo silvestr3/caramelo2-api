76870321d3b92e78dd33f6e68a3612df23f92a92
silvestr3 <fellipe.silvestre@outlook.com>
2024-02-13 13:02:05 +0700

    implement filter bike queryset by storage place

--- api/views/BikeViewSet.py
+++ api/views/BikeViewSet.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets
 from api.serializers import BikeSerializer
-from api.models import Bike
+from api.models import Bike, Storage
+from django.shortcuts import get_object_or_404
 
 class BikeViewSet(viewsets.ModelViewSet):
     """Listing all registered bikes in stock"""
-    queryset = Bike.objects.all().order_by('-id')
     serializer_class = BikeSerializer
+    
+    def get_queryset(self):
+        queryset = Bike.objects.all().order_by('-id')
+
+        storage_id = self.request.query_params.get('storage')
+
+        if storage_id is not None:
+            filterStorage = get_object_or_404(Storage, pk=storage_id)
+            queryset = queryset.filter(storage_place=filterStorage)
+
+        return queryset
+    
 
